a1511539aed8a5a054178eabe7460cf4e249b24c
gurpreet.singh <gurpreet.singh@swstrategies.com>
2019-08-06 14:02:13 +0530

    changes shared module issue

--- src/app/components/search-header/search-header.component.ts
+++ src/app/components/search-header/search-header.component.ts
@@ -7,9 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SearchHeaderComponent implements OnInit {
 
-  @Input() iconImg = '';
-  @Input() iconUser = '';
-  @Input() iconDare = '';
+  @Input() leftIcon = '';
+  @Input() rightIcon = '';
 
   constructor() { }
 
--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from "@angular/core";
 import { FeedsComponent } from './feeds/feeds.component';
 import { SearchHeaderComponent } from './search-header/search-header.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
     declarations: [
         FeedsComponent,
         SearchHeaderComponent,
     ],
+    imports: [
+        IonicModule,
+    ],
     exports: [
         FeedsComponent,
         SearchHeaderComponent,
     ]
 })
-export class SharedModule {}
+export class SharedModule { }
